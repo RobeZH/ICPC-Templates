3e993ccd719b8b22f5e17e9b56d97ee03484413b
RobeZH <541736690@qq.com>
2019-10-20 21:27:50 -0500

    Update Mobius Sieve.cpp

--- Math/Mobius Sieve.cpp
+++ Math/Mobius Sieve.cpp
@@ -4,24 +4,28 @@ typedef long long ll;
 const int M = 100050;
 
 bool p[M];
-int mu[M], pm[M], pms;
+int mu[M], pm[M], phi[M], pms;
 
 void get_mobius_and_sieve(){
     mu[1] = 1;
+    phi[1] = 1;
     fill(p, p + M, true);
     pms = 0;
     for(int i = 2; i < M; i++){
         if(p[i]){
             pm[pms++] = i;
             mu[i] = -1;
+            phi[i] = i - 1;
         }
         for(int j = 0; j < pms && i * pm[j] < M; j++){
             p[i * pm[j]] = false;
             if(i % pm[j] == 0){
                 mu[i * pm[j]] = 0;
+                phi[i * pm[j]] = phi[i] * pm[j];
                 break;
             }
             mu[i * pm[j]] = -mu[i];
+            phi[i * pm[j]] = phi[i] * (pm[j] - 1);
         }
     }
 }
