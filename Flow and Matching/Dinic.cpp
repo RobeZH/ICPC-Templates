27fa66df5b64beda26b91668a20a453e4939dbef
RobeZH <541736690@qq.com>
2019-10-26 18:03:47 -0500

    Update Dinic/KM

--- Flow and Matching/Dinic.cpp
+++ Flow and Matching/Dinic.cpp
@@ -1,9 +1,4 @@
-#include<cstdio>
-#include<iostream>
-#include<vector>
-#include <cstring>
-#include <queue>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 const int N = 100050;
@@ -16,82 +11,62 @@ struct edge{
     }
 };
 
-int n,m,s,d,a,b,c;
-string str;
-vector<edge> G[N];
-char temp[N];
-int level[N], iter[N];
-bool used[N];
 
-void add_edge(int from, int to, int cap){
-    G[from].push_back(edge(to, cap, G[to].size()));
-    G[to].push_back(edge(from, 0, G[from].size() - 1));
-}
+struct Dinic {
+    vector<edge> G[N];
+    int level[N], iter[N];
 
-void bfs(int s){
-    memset(level, -1, sizeof(level));
-    queue<int> que;
-    level[s] = 0;
-    que.push(s);
-    while(!que.empty()){
-        int v = que.front(); que.pop();
-        for(int i = 0; i < G[v].size(); i++){
+    void add_edge(int from, int to, int cap){
+        G[from].push_back(edge(to, cap, G[to].size()));
+        G[to].push_back(edge(from, 0, G[from].size() - 1));
+    }
+
+    void bfs(int s){
+        memset(level, -1, sizeof(level));
+        queue<int> que;
+        level[s] = 0;
+        que.push(s);
+        while(!que.empty()){
+            int v = que.front(); que.pop();
+            for(int i = 0; i < G[v].size(); i++){
+                edge &e = G[v][i];
+                if(e.cap > 0 && level[e.to] < 0){
+                    level[e.to] = level[v] + 1;
+                    que.push(e.to);
+                }
+            }
+        }
+    }
+
+
+    int dfs(int v, int t, int f){
+        if(v == t) return f;
+
+        for(int &i = iter[v]; i < G[v].size(); i++){
             edge &e = G[v][i];
-            if(e.cap > 0 && level[e.to] < 0){
-                level[e.to] = level[v] + 1;
-                que.push(e.to);
+            if(e.cap > 0 && level[v] < level[e.to]){
+                int d = dfs(e.to, t, min(e.cap, f));
+                if(d > 0){
+                    e.cap -= d;
+                    G[e.to][e.rev].cap += d;
+                    return d;
+                }
+            }
+        }
+        return 0;
+    }
+
+
+    int max_flow(int s, int t){
+        int flow = 0;
+        for(;;){
+            bfs(s);
+            if(level[t] < 0) return flow;
+            memset(iter, 0, sizeof(iter));
+            int f;
+            while((f = dfs(s, t, INF)) > 0){
+                flow += f;
             }
         }
     }
-}
-
-
-int dfs(int v, int t, int f){
-    if(v == t) return f;
-
-    for(int &i = iter[v]; i < G[v].size(); i++){
-        edge &e = G[v][i];
-        if(e.cap > 0 && level[v] < level[e.to]){
-            int d = dfs(e.to, t, min(e.cap, f));
-            if(d > 0){
-                e.cap -= d;
-                G[e.to][e.rev].cap += d;
-                return d;
-            }
-        }
-    }
-    return 0;
-}
-
-
-int max_flow(int s, int t){
-    int flow = 0;
-    for(;;){
-        bfs(s);
-        if(level[t] < 0) return flow;
-        memset(iter, 0, sizeof(iter));
-        int f;
-        while((f = dfs(s, t, INF)) > 0){
-            flow += f;
-        }
-    }
-}
-
-int main(){
-    while(scanf("%d%d", &n, &m) != EOF){
-        for(int i = 0; i < N; i++) G[i].clear();
-        int s = n + m, t = s + 1;
-        for(int i = 0; i < n; i++){
-            add_edge(s, i, 1);
-            for(int j = 0; j < m; j++){
-                scanf("%d", &a);
-                if(a == 1) add_edge(i, j + n, 1);
-            }
-        }
-        for(int i = 0; i < m; i++){
-            scanf("%d", &a);
-            add_edge(i + n, t, a);
-        }
-        cout << (max_flow(s, t) == n ? "YES" : "NO") << endl;
-    }
-}
+} dinic;
--- Flow and Matching/KM (Maximum Weight Matching).cpp
+++ Flow and Matching/KM (Maximum Weight Matching).cpp
@@ -1,111 +1,50 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-
+#include <bits/stdc++.h>
 using namespace std;
+const int MAXN = 2010;
+const int oo = 1000000007;
 
-const int N = 505;
-const int INF = (int)1e9;
-
-string str[N];
-int hx[N], hy[N], mx[N], my[N], hi=0, mi=0;
-int n,m,nx,ny;
-int match[N],lx[N],ly[N],slack[N];
-int visx[N],visy[N],w[N][N];
-
-int abs(int a){
-    return a > 0 ? a : -a;
+int dist[MAXN][MAXN];
+// Finding the minimum weight prefect matching (of size n) in O(N^3)
+// The dist matrix is 1-indexed.
+int hungarian(int n, int m){
+	vector<int> u(n + 1), v(m + 1), p(m + 1), way(m + 1);
+	for(int i = 1; i <= n; i++){
+		p[0] = i;
+		int j0 = 0;
+		vector<int> minv(m + 1, oo);
+		vector<char> used(m + 1, false);
+		do{
+			used[j0] = true;
+			int i0 = p[j0], delta = oo, j1;
+			for(int j = 1; j <= m; j++){
+				if(!used[j]){
+					int cur = dist[i0][j] - u[i0] - v[j];
+					if(cur < minv[j]){
+						minv[j] = cur;
+						way[j] = j0;
+					}
+					if(minv[j] < delta){
+						delta = minv[j];
+						j1 = j;
+					}
+				}
+			}
+			for(int j = 0; j <= m; j++){
+				if(used[j]){
+					u[p[j]] += delta;
+					v[j] -= delta;
+				} else {
+					minv[j] -= delta;
+				}
+			}
+			j0 = j1;
+		} while (p[j0] != 0);
+		do{
+			int j1 = way[j0];
+			p[j0] = p[j1];
+			j0 = j1;
+		} while(j0);
+	}
+	return -v[0];
 }
 
-int dis(int h, int m){
-    return abs(hx[h] - mx[m]) + abs(hy[h] - my[m]);
-}
-
-int dfs(int x){
-    visx[x]=1;
-    for(int y=1;y<=ny;y++){
-        if(visy[y])
-            continue;
-        int tmp=lx[x]+ly[y]-w[x][y];
-        if(tmp==0){
-            visy[y]=1;
-            if(match[y]==-1 || dfs(match[y])){
-                match[y]=x;
-                return 1;
-            }
-        }else if(slack[y]>tmp){
-            slack[y]=tmp;
-        }
-    }
-    return 0;
-}
-
-int KM(bool max_w){
-    for(int i = 0; i < N; i++) match[i] = -1;
-    if(!max_w){
-        for(int i = 1; i <= nx; i++)
-            for(int j = 1; j <= ny;j++)
-                w[i][j] = -w[i][j];
-
-    }
-    int i,j;
-
-    memset(ly,0,sizeof(ly));
-    memset(lx,0, sizeof(lx));
-    for(int i = 0; i < N; i++) hx[i]= hy[i] = mx[i] = my[i] = ly[i] = lx[i] = 0;
-    for(i=1;i<=nx;i++)
-        for(j=1,lx[i]=-INF;j<=ny;j++)
-            if(w[i][j]>lx[i])
-                lx[i]=w[i][j];
-    for(int x=1;x<=nx;x++){
-        for(i=1;i<=ny;i++)
-            slack[i]=INF;
-        while(1){
-            memset(visx,0,sizeof(visx));
-            memset(visy,0,sizeof(visy));
-            if(dfs(x))
-                break;
-            int d=INF;
-            for(i=1;i<=ny;i++)
-                if(!visy[i] && d>slack[i])
-                    d=slack[i];
-            for(i=1;i<=nx;i++)
-                if(visx[i])
-                    lx[i]-=d;
-            for(i=1;i<=ny;i++)
-                if(visy[i])
-                    ly[i]+=d;
-                else
-                    slack[i]-=d;
-        }
-    }
-    int res=0;
-    for(i=1;i<=ny;i++)
-        if(match[i]!=-1)
-            res+=w[match[i]][i];
-    if(!max_w) return -res;
-    else return res;
-}
-int main(){
-    while(true){
-        cin >> n >> m;
-        if(n == 0 && m == 0) break;
-        for(int i = 0; i < n; i++){
-            cin >> str[i];
-            for(int j = 0; j < m; j++){
-                if(str[i][j] == 'H') hx[hi] = i, hy[hi++] = j;
-                if(str[i][j] == 'm') mx[mi] = i, my[mi++] = j;
-            }
-        }
-
-        for(int i = 0; i < hi; i++){
-            for(int j = 0; j < hi; j++){
-                w[i+1][j+1] = dis(i, j);
-            }
-        }
-        nx=ny=n=hi;
-        int ans=KM(false);
-        printf("%d\n",ans);
-    }
-    return 0;
-}
